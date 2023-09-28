aa4b38e891d8924160c1be8a368bad63d2e33c6b
CodeLegalist123 <146299120+CodeLegalist123@users.noreply.github.com>
2023-09-28 15:42:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ function displayCards(filteredData) {
                 <a href="#">
                     <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${item.title}</h5>
                 </a>
-                <a href="#" class="w-full inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <a href="${item.link}" class="w-full inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Read more
                 </a>
             </div>
